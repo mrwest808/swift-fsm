aed768b6668cdf0baf3da855864458ec695da60b
Johan <johan.west@viablelab.com>
2020-04-29 00:01:11 +0200

    Add FSM source & tests

--- Sources/FSM/FSM.swift
+++ Sources/FSM/FSM.swift
@@ -1,3 +1,38 @@
-struct FSM {
-    var text = "Hello, World!"
+//
+//  FSM.swift
+//
+//
+//  Created by Johan West on 2020-04-28.
+//
+
+public final class StateMachine<State, Event> {
+  public typealias StateEventMapper = (State, Event) -> State?
+  public typealias OnTransitionClosure = (Event) -> Void
+
+  private var stateEventMapper: StateEventMapper
+  private var onTransitionClosure: OnTransitionClosure?
+
+  public var currentState: State
+  public var previousState: State
+
+  init(initial: State, _ stateEventMapper: @escaping StateEventMapper) {
+    self.stateEventMapper = stateEventMapper
+    currentState = initial
+    previousState = initial
+  }
+
+  public func send(_ event: Event) {
+    if let nextState = stateEventMapper(currentState, event) {
+      previousState = currentState
+      currentState = nextState
+
+      if let onTransitionClosure = self.onTransitionClosure {
+        onTransitionClosure(event)
+      }
+    }
+  }
+
+  public func on(_ onTransitionClosure: @escaping OnTransitionClosure) {
+    self.onTransitionClosure = onTransitionClosure
+  }
 }
--- Tests/FSMTests/FSMTests.swift
+++ Tests/FSMTests/FSMTests.swift
@@ -1,15 +1,143 @@
 import XCTest
 @testable import FSM
 
+struct TrafficMachine {
+  enum State {
+    case green
+    case yellow
+    case red
+  }
+
+  enum Event {
+    case timer
+  }
+
+  let machine = StateMachine<State, Event>(initial: .green) { state, _ in
+    switch state {
+    case .green:
+      return .yellow
+    case .yellow:
+      return .red
+    case .red:
+      return .green
+    }
+  }
+}
+
+struct SimpleMachine {
+  enum State {
+    case sitting
+    case standing
+  }
+
+  enum Event {
+    case sit
+    case stand
+  }
+
+  let machine = StateMachine<State, Event>(initial: .standing) { state, event in
+    switch (state, event) {
+    case (.sitting, .stand):
+      return .standing
+    case (.standing, .sit):
+      return .sitting
+    default:
+      return nil
+    }
+  }
+}
+
+struct AssociatedValuesMachine {
+  enum State: Equatable {
+    case idle
+    case editing(Int)
+
+    // For the sake of equality checking within the test suite
+    static func ==(lhs: State, rhs: State) -> Bool {
+      switch (lhs, rhs) {
+      case (let .editing(lhsInt), let .editing(rhsInt)):
+        return lhsInt == rhsInt
+      case (.idle, .idle):
+        return true
+      default:
+        return lhs == rhs
+      }
+    }
+  }
+
+  enum Event {
+    case edit(Int)
+    case save
+  }
+
+  let machine = StateMachine<State, Event>(initial: .idle) { state, event in
+    switch (state, event) {
+    case (.idle, let .edit(id)):
+      return .editing(id)
+    case (.editing(_), .save):
+      return .idle
+    default:
+      return nil
+    }
+  }
+}
+
 final class FSMTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FSM().text, "Hello, World!")
+  func testTrafficMachine() {
+    let trafficMachine = TrafficMachine()
+    let machine = trafficMachine.machine
+
+    XCTAssertEqual(machine.currentState, TrafficMachine.State.green)
+    machine.send(.timer)
+    XCTAssertEqual(machine.currentState, TrafficMachine.State.yellow)
+    machine.send(.timer)
+    XCTAssertEqual(machine.currentState, TrafficMachine.State.red)
+    machine.send(.timer)
+    XCTAssertEqual(machine.currentState, TrafficMachine.State.green)
+  }
+
+  func testSimpleMachine() {
+    let simpleMachine = SimpleMachine()
+    let machine = simpleMachine.machine
+    var lastEvent: SimpleMachine.Event?
+
+    machine.on { event in
+      lastEvent = event
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    XCTAssertEqual(lastEvent, nil)
+    XCTAssertEqual(machine.currentState, SimpleMachine.State.standing)
+    machine.send(.stand)
+    XCTAssertEqual(lastEvent, nil)
+    XCTAssertEqual(machine.currentState, SimpleMachine.State.standing)
+    machine.send(.sit)
+    XCTAssertEqual(lastEvent, SimpleMachine.Event.sit)
+    XCTAssertEqual(machine.currentState, SimpleMachine.State.sitting)
+    machine.send(.sit)
+    XCTAssertEqual(machine.currentState, SimpleMachine.State.sitting)
+    machine.send(.stand)
+    XCTAssertEqual(lastEvent, SimpleMachine.Event.stand)
+    XCTAssertEqual(machine.currentState, SimpleMachine.State.standing)
+  }
+
+  func testAssociatedValuesMachine() {
+    let associatedValuesMachine = AssociatedValuesMachine()
+    let machine = associatedValuesMachine.machine
+
+    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.idle)
+    machine.send(.save)
+    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.idle)
+    machine.send(.edit(1))
+    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.editing(1))
+    machine.send(.edit(2))
+    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.editing(1))
+    machine.send(.save)
+    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.idle)
+  }
+
+  static var allTests = [
+    ("testTrafficMachine", testTrafficMachine),
+    ("testSimpleMachine", testSimpleMachine),
+    ("testAssociatedValuesMachine", testAssociatedValuesMachine),
+  ]
 }
