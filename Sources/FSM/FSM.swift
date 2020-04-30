c3362386c044a9260b4614df8f0c0849791e9af8
Johan <johan.west@viablelab.com>
2020-04-30 22:31:13 +0200

    Update API of StateMachine and add ObservableStateMachine

--- Sources/FSM/FSM.swift
+++ Sources/FSM/FSM.swift
@@ -6,12 +6,16 @@
 //  Created by Johan West on 2020-04-28.
 //
 
+import Combine
+
 public final class StateMachine<State, Event> {
   public typealias StateEventMapper = (State, Event) -> State?
-  public typealias OnTransitionClosure = (Event) -> Void
+  public typealias OnStateTransitionClosure = (State, Event) -> Void
+  public typealias OnEventClosure = (Event, State) -> Void
 
   private var stateEventMapper: StateEventMapper
-  private var onTransitionClosure: OnTransitionClosure?
+  private var onStateTransitionClosure: OnStateTransitionClosure?
+  private var onEventClosure: OnEventClosure?
 
   public var currentState: State
   public var previousState: State
@@ -27,13 +31,48 @@ public final class StateMachine<State, Event> {
       previousState = currentState
       currentState = nextState
 
-      if let onTransitionClosure = self.onTransitionClosure {
-        onTransitionClosure(event)
+      if let onStateTransitionClosure = self.onStateTransitionClosure {
+        onStateTransitionClosure(currentState, event)
       }
+    }
+
+    if let onEventClosure = self.onEventClosure {
+      onEventClosure(event, currentState)
     }
   }
 
-  public func on(_ onTransitionClosure: @escaping OnTransitionClosure) {
-    self.onTransitionClosure = onTransitionClosure
+  public func onTransition(_ onStateTransitionClosure: @escaping OnStateTransitionClosure) {
+    self.onStateTransitionClosure = onStateTransitionClosure
+  }
+
+  public func onEvent(_ onEventClosure: @escaping OnEventClosure) {
+    self.onEventClosure = onEventClosure
+  }
+}
+
+@available(OSX 10.15, *)
+public final class ObservableStateMachine<State, Event>: ObservableObject {
+  public typealias StateEventMapper = (State, Event) -> State?
+
+  private var stateEventMapper: StateEventMapper
+
+  @Published var currentState: State
+  @Published var previousState: State
+
+  var eventPublisher = PassthroughSubject<Event, Never>()
+
+  public init(initial: State, _ stateEventMapper: @escaping StateEventMapper) {
+    self.stateEventMapper = stateEventMapper
+    currentState = initial
+    previousState = initial
+  }
+
+  public func send(_ event: Event) {
+    if let nextState = stateEventMapper(currentState, event) {
+      previousState = currentState
+      currentState = nextState
+    }
+
+    eventPublisher.send(event)
   }
 }
--- Tests/FSMTests/FSMTests.swift
+++ Tests/FSMTests/FSMTests.swift
@@ -84,61 +84,173 @@ extension AssociatedValuesMachine.State: Equatable {
   }
 }
 
+@available(OSX 10.15, *)
+struct ObservableCountMachine {
+  enum State {
+    case count(Int)
+  }
+
+  enum Event {
+    case increment
+    case decrement
+    case add(Int)
+  }
+
+  let machine = ObservableStateMachine<State, Event>(initial: .count(0)) { state, event in
+    switch (state, event) {
+    case (let .count(n), .increment):
+      return .count(n + 1)
+    case (let .count(n), .decrement):
+      return .count(n - 1)
+    case (let .count(n), let .add(amount)):
+      return .count(n + amount)
+    }
+  }
+}
+
+// Needed for equality assertions in tests below
+
+@available(OSX 10.15, *)
+extension ObservableCountMachine.State: Equatable {
+  static func ==(lhs: ObservableCountMachine.State, rhs: ObservableCountMachine.State) -> Bool {
+    switch (lhs, rhs) {
+    case (let .count(lhsInt), let .count(rhsInt)):
+      return lhsInt == rhsInt
+    }
+  }
+}
+
+@available(OSX 10.15, *)
+extension ObservableCountMachine.Event: Equatable {
+  static func ==(lhs: ObservableCountMachine.Event, rhs: ObservableCountMachine.Event) -> Bool {
+    switch (lhs, rhs) {
+    case (let .add(lhsInt), let .add(rhsInt)):
+      return lhsInt == rhsInt
+    case (.increment, .increment), (.decrement, .decrement):
+      return true
+    default:
+      return false
+    }
+  }
+}
+
 final class FSMTests: XCTestCase {
   func testTrafficMachine() {
+    typealias S = TrafficMachine.State
+
     let trafficMachine = TrafficMachine()
     let machine = trafficMachine.machine
 
-    XCTAssertEqual(machine.currentState, TrafficMachine.State.green)
+    XCTAssertEqual(machine.currentState, S.green)
     machine.send(.timer)
-    XCTAssertEqual(machine.currentState, TrafficMachine.State.yellow)
+    XCTAssertEqual(machine.currentState, S.yellow)
     machine.send(.timer)
-    XCTAssertEqual(machine.currentState, TrafficMachine.State.red)
+    XCTAssertEqual(machine.currentState, S.red)
     machine.send(.timer)
-    XCTAssertEqual(machine.currentState, TrafficMachine.State.green)
+    XCTAssertEqual(machine.currentState, S.green)
   }
 
   func testSimpleMachine() {
+    typealias S = SimpleMachine.State
+    typealias E = SimpleMachine.Event
+
     let simpleMachine = SimpleMachine()
     let machine = simpleMachine.machine
-    var lastEvent: SimpleMachine.Event?
-    var onCallCount = 0
+    var lastTransitionEvent: E?
+    var lastEvent: E?
+    var onStateTransitionCallCount = 0
+    var onEventCallCount = 0
 
-    machine.on { event in
+    machine.onTransition { _, event in
+      lastTransitionEvent = event
+      onStateTransitionCallCount += 1
+    }
+
+    machine.onEvent { event, _ in
       lastEvent = event
-      onCallCount += 1
+      onEventCallCount += 1
     }
 
     XCTAssertEqual(lastEvent, nil)
-    XCTAssertEqual(onCallCount, 0)
-    XCTAssertEqual(machine.currentState, SimpleMachine.State.standing)
+    XCTAssertEqual(lastTransitionEvent, nil)
+    XCTAssertEqual(onEventCallCount, 0)
+    XCTAssertEqual(onStateTransitionCallCount, 0)
+    XCTAssertEqual(machine.currentState, S.standing)
     machine.send(.stand)
-    XCTAssertEqual(lastEvent, nil)
-    XCTAssertEqual(machine.currentState, SimpleMachine.State.standing)
+    XCTAssertEqual(lastEvent, E.stand)
+    XCTAssertEqual(lastTransitionEvent, nil)
+    XCTAssertEqual(machine.currentState, S.standing)
     machine.send(.sit)
-    XCTAssertEqual(lastEvent, SimpleMachine.Event.sit)
-    XCTAssertEqual(machine.currentState, SimpleMachine.State.sitting)
+    XCTAssertEqual(lastEvent, E.sit)
+    XCTAssertEqual(machine.currentState, S.sitting)
     machine.send(.sit)
-    XCTAssertEqual(machine.currentState, SimpleMachine.State.sitting)
+    XCTAssertEqual(machine.currentState, S.sitting)
     machine.send(.stand)
-    XCTAssertEqual(lastEvent, SimpleMachine.Event.stand)
-    XCTAssertEqual(onCallCount, 2)
-    XCTAssertEqual(machine.currentState, SimpleMachine.State.standing)
+    XCTAssertEqual(lastTransitionEvent, E.stand)
+    XCTAssertEqual(onEventCallCount, 4)
+    XCTAssertEqual(onStateTransitionCallCount, 2)
+    XCTAssertEqual(machine.currentState, S.standing)
   }
 
   func testAssociatedValuesMachine() {
+    typealias S = AssociatedValuesMachine.State
+
     let associatedValuesMachine = AssociatedValuesMachine()
     let machine = associatedValuesMachine.machine
 
-    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.idle)
+    XCTAssertEqual(machine.currentState, S.idle)
     machine.send(.save)
-    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.idle)
+    XCTAssertEqual(machine.currentState, S.idle)
     machine.send(.edit(1))
-    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.editing(1))
+    XCTAssertEqual(machine.currentState, S.editing(1))
     machine.send(.edit(2))
-    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.editing(1))
+    XCTAssertEqual(machine.currentState, S.editing(1))
     machine.send(.save)
-    XCTAssertEqual(machine.currentState, AssociatedValuesMachine.State.idle)
+    XCTAssertEqual(machine.currentState, S.idle)
+  }
+
+  @available(OSX 10.15, *)
+  func testObservableCountMachine() {
+    typealias S = ObservableCountMachine.State
+    typealias E = ObservableCountMachine.Event
+
+    let observableCountMachine = ObservableCountMachine()
+    let machine = observableCountMachine.machine
+    var observedCurrentState = machine.currentState
+    var observedPreviousState = machine.previousState
+    var observedEvents = [E]()
+
+    let disposables = [
+      machine.$currentState.sink { state in
+        observedCurrentState = state
+      },
+      machine.$previousState.sink { state in
+        observedPreviousState = state
+      },
+      machine.eventPublisher.sink { event in
+        observedEvents.append(event)
+      }
+    ]
+
+    XCTAssertEqual(observedCurrentState, S.count(0))
+    XCTAssertEqual(observedPreviousState, S.count(0))
+    XCTAssertEqual(observedEvents, [])
+    XCTAssertEqual(machine.currentState, S.count(0))
+    machine.send(.increment)
+    XCTAssertEqual(machine.currentState, S.count(1))
+    machine.send(.increment)
+    XCTAssertEqual(machine.currentState, S.count(2))
+    machine.send(.decrement)
+    XCTAssertEqual(machine.currentState, S.count(1))
+    machine.send(.add(10))
+    XCTAssertEqual(machine.currentState, S.count(11))
+    XCTAssertEqual(observedCurrentState, S.count(11))
+    XCTAssertEqual(observedPreviousState, S.count(1))
+    XCTAssertEqual(observedEvents, [.increment, .increment, .decrement, .add(10)])
+
+    for disposable in disposables {
+      disposable.cancel()
+    }
   }
 
   static var allTests = [
