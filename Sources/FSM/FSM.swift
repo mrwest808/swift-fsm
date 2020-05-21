95b303e7ea57efd282ad3f2109983e6a7471ec5e
Johan <johan.west@viablelab.com>
2020-05-21 13:00:49 +0200

    Add Unstable_ObservableEventPublisher & Unstable_ObservableStateMachine

--- Sources/FSM/FSM.swift
+++ Sources/FSM/FSM.swift
@@ -75,3 +75,232 @@ public final class ObservableStateMachine<State, Event>: ObservableObject {
     eventPublisher.send(event)
   }
 }
+
+@available(OSX 10.15, *)
+public class Unstable_ObservableEventPublisher<Event>: ObservableObject {
+  typealias Unsubscriber = () -> Void
+
+  static func subscribe(to disposables: [AnyCancellable]) -> Unsubscriber {
+    return {
+      for disposable in disposables {
+        disposable.cancel()
+      }
+    }
+  }
+
+  public final var eventPublisher = PassthroughSubject<Event, Never>()
+
+  public func send(_ event: Event) {
+    receive(event: event)
+    eventPublisher.send(event)
+  }
+
+  func receive(event: Event) {}
+}
+
+@available(OSX 10.15, *)
+public class Unstable_ObservableStateMachine<State, Event>: Unstable_ObservableEventPublisher<Event> {
+  public typealias StateEventMapper = (State, Event) -> State?
+
+  private var stateEventMapper: StateEventMapper
+
+  @Published public final var currentState: State
+  @Published public final var previousState: State
+
+  public init(initial: State, _ stateEventMapper: @escaping StateEventMapper) {
+    self.stateEventMapper = stateEventMapper
+    currentState = initial
+    previousState = initial
+  }
+
+  public override func send(_ event: Event) {
+    if let nextState = stateEventMapper(currentState, event) {
+      previousState = currentState
+      currentState = nextState
+      receive(transitionBy: event, to: currentState, from: previousState)
+    }
+
+    super.send(event)
+  }
+
+  func receive(transitionBy event: Event, to: State, from: State) {}
+}
+
+// =============================================================================
+// EXPERIMENTAL NEW API
+// TODO: Add tests
+// =============================================================================
+//
+//public class ObservableEventPublisher<Event>: ObservableObject {
+//  typealias Unsubscriber = () -> Void
+//
+//  static func subscribe(to disposables: [AnyCancellable]) -> Unsubscriber {
+//    return {
+//      for disposable in disposables {
+//        disposable.cancel()
+//      }
+//    }
+//  }
+//
+//  public final var eventPublisher = PassthroughSubject<Event, Never>()
+//
+//  public func send(_ event: Event) {
+//    receive(event: event)
+//    eventPublisher.send(event)
+//  }
+//
+//  func receive(event: Event) {}
+//}
+//
+//public class ObservableStateMachine<State, Event>: ObservableEventPublisher<Event> {
+//  public typealias StateEventMapper = (State, Event) -> State?
+//
+//  private var stateEventMapper: StateEventMapper
+//
+//  @Published public final var currentState: State
+//  @Published public final var previousState: State
+//
+//  public init(initial: State, _ stateEventMapper: @escaping StateEventMapper) {
+//    self.stateEventMapper = stateEventMapper
+//    currentState = initial
+//    previousState = initial
+//  }
+//
+//  public override func send(_ event: Event) {
+//    if let nextState = stateEventMapper(currentState, event) {
+//      previousState = currentState
+//      currentState = nextState
+//      receive(transitionBy: event, to: currentState, from: previousState)
+//    }
+//
+//    super.send(event)
+//  }
+//
+//  func receive(transitionBy event: Event, to: State, from: State) {}
+//}
+//
+// =============================================================================
+// USAGE EXAMPLES:
+// =============================================================================
+//
+//final class CounterState: ObservableEventPublisher<CounterState.Event> {
+//  enum Event {
+//    case increment
+//  }
+//
+//  @Published var count = 0
+//
+//  override func receive(event: Event) {
+//    switch event {
+//    case .increment:
+//      count += 1
+//    }
+//  }
+//}
+//
+//let counter = CounterState()
+//var unsubscribe: CounterState.Unsubscriber
+//
+//unsubscribe = CounterState.subscribe(to: [
+//  counter.eventPublisher.sink { event in
+//    print("Event: \(event), Count: \(counter.count)")
+//  }
+//])
+//
+//print("Count: \(counter.count)")
+//counter.send(.increment)
+//print("Count: \(counter.count)")
+//
+//unsubscribe()
+//
+//final class TrafficLightMachine: ObservableStateMachine<TrafficLightMachine.State, TrafficLightMachine.Event> {
+//  enum State {
+//    case green
+//    case yellow
+//    case red
+//  }
+//
+//  enum Event {
+//    case timer
+//  }
+//
+//  @Published var rounds = 0
+//  var yellowCount = 0
+//
+//  init() {
+//    super.init(initial: .green) { state, _ in
+//      switch (state) {
+//      case .green:
+//        return .yellow
+//      case .yellow:
+//        return .red
+//      case .red:
+//        return .green
+//      }
+//    }
+//  }
+//
+//  override func receive(transitionBy event: Event, to: State, from: State) {
+//    if to == .green {
+//      rounds += 1
+//    }
+//
+//    if to == .yellow {
+//      yellowCount += 1
+//    }
+//  }
+//}
+//
+//let trafficLight = TrafficLightMachine()
+//
+//print("\n===\n")
+//
+//print("Yellow count: \(trafficLight.yellowCount)")
+//
+//for iteration in 1...10 {
+//  if iteration == 1 {
+//    print("Light: \(trafficLight.currentState) (rounds: \(trafficLight.rounds))")
+//  }
+//
+//  trafficLight.send(.timer)
+//  print("Light: \(trafficLight.currentState) (rounds: \(trafficLight.rounds))")
+//}
+//
+//print("Yellow count: \(trafficLight.yellowCount)")
+//
+//final class HumanMachine: ObservableStateMachine<HumanMachine.State, HumanMachine.Event> {
+//  enum State {
+//    case standing
+//    case sitting
+//  }
+//
+//  enum Event {
+//    case sit
+//    case stand
+//  }
+//
+//  init() {
+//    super.init(initial: .standing) { state, event in
+//      switch (state, event) {
+//      case (.standing, .sit):
+//        return .sitting
+//      case (.sitting, .stand):
+//        return .standing
+//      default:
+//        return nil
+//      }
+//    }
+//  }
+//}
+//
+//let human = HumanMachine()
+//
+//print("\n===\n")
+//
+//print("State: \(human.currentState)")
+//human.send(.stand)
+//print("State: \(human.currentState)")
+//human.send(.sit)
+//print("State: \(human.currentState)")
+//human.send(.stand)
+//print("State: \(human.currentState)")
